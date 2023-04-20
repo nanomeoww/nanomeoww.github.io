6baca328db43c35a4894ef1787f77d3857cdb51e
nanomeoww <nano_meow@icloud.com>
2023-04-20 12:15:43 +0800

    Site updated: 2023-04-20 12:15:43

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/04/20/hello-world/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["/75ae45b8.html","/bd3c650b.html","/a1447d9.html","/13f29456.html","/4a17b156.html","/e7b3bb5c.html","/0.html","/b28ca4a7.html","/89757140.html","/1f63b7d1.html","/40245f02.html","/9db656e5.html","/50962.html","/2e066104.html","/20cf26ba.html","/644ceca6.html","/e2e5eeac.html","/2ecbcba0.html","/79abcd0d.html","/a2b99b97.html","/cf8e3b18.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
